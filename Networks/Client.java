fbd3317de7cb2fedaadb8028a47a8b382fe6d7cd
vamshireddyvem <vamshireddy.vem@gmail.com>
2025-07-22 00:53:27 -0500

    networks

---
+++ Networks/Client.java
@@ -0,0 +1,29 @@
+package Networks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket st= new Socket("Localhost", 2000);
+            BufferedReader keybr= new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader br = new BufferedReader(new InputStreamReader(st.getInputStream())); 
+            PrintStream ps= new PrintStream(st.getOutputStream());
+            String msg;
+            do {
+                msg=keybr.readLine();
+                ps.print(msg);
+                msg=br.readLine();
+                System.out.println("From Client "+msg);
+            } while (!msg.equals("nde"));
+            st.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Networks/Server.java
@@ -0,0 +1,35 @@
+package Networks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+       
+        try {
+        ServerSocket ss= new ServerSocket();
+        Socket st = ss.accept();
+        
+        BufferedReader br= new BufferedReader(new InputStreamReader(st.getInputStream()));
+        PrintStream ps = new PrintStream(st.getOutputStream());
+
+        String msg;
+        StringBuilder sbr;
+            do {
+                msg=br.readLine();
+                sbr= new StringBuilder(msg);
+                sbr.reverse();
+                msg=sbr.toString();
+                ps.println(msg);
+            } while (!msg.equals("dne"));
+                st.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
