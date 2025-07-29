43ebad0a7a19beea2fc18c088cf5d4adede9fd8d
vamshireddyvem <vamshireddy.vem@gmail.com>
2025-07-29 13:52:38 -0500

    dir changes for gradle

---
+++ src/main/java/Generics/intro.java
@@ -0,0 +1,19 @@
+package Generics;
+
+public class intro {
+    public static void main(String[] args) {
+        // Object obj = new String();
+        // obj= "Hi";
+        // obj= 10;// Doesnt show any Compile time Errors as their is No Type Safety
+        // String str= (String) obj; // type cast is needed when try to store/Use in a variable
+        // System.out.println(obj);
+
+        Object obj[]= new Object[3];
+        obj[0]="Hi";
+        obj[1]="Hello";
+        obj[2]=25;
+        for (int i=0; i<3;i++) {
+            String str= (String) obj[i];
+            System.out.println(str);        }
+    }
+}
